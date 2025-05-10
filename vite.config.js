79b8eb636974180b435f6991c4bfbda11287dd27
mk293822 <mkt293822@gmail.com>
2025-05-10 17:31:23 +0000

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        outDir: "dist",
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: "resources/js/app.tsx",
             refresh: true,
         }),
         react(),
